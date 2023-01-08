c87a5ac8eb506da44fde7d04b59135585e81e20f
Daiv Boveri <daiv.boveri@gmail.com>
2023-01-08 12:01:56 -0600

    Task: learning how to define props Implemented "say-hello" with "greeting" component

---
+++ src/components/greeting.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+// Defining the <Greeting> component with props
+const Greeting = (props) => {
+    return (
+        <p>Hi {props.name}!</p>
+    )
+}
+
+export default Greeting
