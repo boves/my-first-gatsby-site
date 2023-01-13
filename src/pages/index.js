d72abd979b33e2c53fd91b7acaa9d0f2661c4c08
Daiv Boveri <daiv.boveri@gmail.com>
2023-01-13 13:33:58 -0600

    Task: use useStaticQuery to create Seo component
    I created an Seo component, imported it into Layout
    component, and refactored the Title tag in both
    pages into a <Seo title="Lorem" /> tag.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
+import Seo from '../components/seo'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
+
       <p>Welcome to my Gatsby site!</p>
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
@@ -19,7 +21,7 @@ const IndexPage = () => {
 }
 
 // You'll learn about this in the next task, just copy it for now
-export const Head = () => <title>Home Page</title>
+export const Head = () => <Seo title ="Home Page" />
 
 // Step 3: Export your component
 export default IndexPage
