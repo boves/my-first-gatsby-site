43daf1d3c1ea55d9adcd7a78e6fa89d4b02c5376
Daiv Boveri <daiv.boveri@gmail.com>
2023-01-14 11:58:38 -0600

    Task: adding content to blog pages

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,35 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 
-const BlogPage = () => {
+
+const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle={"My Blog Posts"}>
-            <p>My cool posts will go in here.</p>
+            <p>My cool blog posts are right here: </p>
+            <ul>
+                {
+                    data.allFile.nodes.map(node => (
+                        <li key={node.name}>
+                            {node.name}
+                        </li>
+                    ))
+                }
+            </ul>
         </Layout>
     )
 }
+
+export const query = graphql`
+query MyQuery {
+    allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
+      nodes {
+        name
+      }
+    }
+  }
+`
 
 export const Head = () => <Seo title="My Blog Posts" />
 
