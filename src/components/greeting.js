8b10b17ccbadd1505e5cc03885b8d01d8a45068f
Daiv Boveri <daiv.boveri@gmail.com>
2023-01-14 18:00:03 -0600

    Task: removing unnecessary files

--- src/components/greeting.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-// Defining the <Greeting> component with props
-const Greeting = (props) => {
-    return (
-        <p>Hi {props.name}!</p>
-    )
-}
-
-export default Greeting
